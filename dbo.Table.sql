9541d05e02f7fffd3fd45f38ab2b405443b65974
MYChopdat <71784945+snipersnooze@users.noreply.github.com>
2022-06-23 16:36:28 +0200

    Files for service

---
+++ dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Shipping] (
+    [User_ID]           INT          IDENTITY (1, 1) NOT NULL,
+    [Ship_Address]      VARCHAR (50) NOT NULL,
+    [Ship_Instructions] VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([User_ID] ASC),
+    FOREIGN KEY ([User_ID]) REFERENCES [dbo].[User]
+);
